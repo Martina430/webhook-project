5f2236c4f0a5405a9dfbdde70b71d290bd416318
Martina430 <martina.anzani@icloud.com>
2025-05-27 01:11:19 +0200

    Add files via upload

--- server.R
+++ server.R
@@ -1,12 +1,5 @@
 library(plumber)
 print("Avviando il server Webhook...")  
-r <- plumb("webhook.R")  # Collega il file principale
+r <- plumb("webhook.R")  
 print("Webhook caricato!")  
-r$run(port = 8000)  # Avvia il server sulla porta 8000
-
-source("server.R")
-
-
-library(httr)
-res <- POST("http://127.0.0.1:8000/webhook", body = '{"test": "Instagram"}', encode = "json")
-content(res)
+r$run(host = "0.0.0.0", port = as.numeric(Sys.getenv("PORT", "8000")))
